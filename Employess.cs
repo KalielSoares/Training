52b36c94720c70b070bbd33323fe455aecfed4b5
Kaliel Soares <kalielsoares6@gmail.com>
2024-12-07 14:31:28 -0300

    Adicionar arquivos de projeto.

---
+++ Employess.cs
@@ -0,0 +1,23 @@
+﻿namespace Treinamento {
+    public class Employee {
+
+        private int Id { get; set; }
+        private string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id,string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+
+        public double SALARY => Salary;
+        public int ID => Id;
+
+        public override string ToString() {
+            return Id + ", "  + Name + ", " + Salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Treinamento {
+    public class Program {
+        static void Main(string[] args) {
+            List<Employee> employees = new List<Employee>();
+
+
+            Console.WriteLine("How many employees will be registred?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Emplyoee #" + i);
+                Console.WriteLine("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                employees.Add(new Employee(id, name, salary));
+            }
+
+            Console.WriteLine("Enter the employee id that have salary increase: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Employee b = employees.Find(x => x.ID == a);
+            
+            if (b == null) {
+                Console.WriteLine("This id does not exist");
+            }
+
+            Console.WriteLine("Enter the percentage");
+            double percentage = double.Parse(Console.ReadLine());
+
+            Employee s = employees.Find(x => x.ID == a);
+            poe
+            if (s != null)
+            {
+                s.Salary += s.Salary * (percentage / 100);
+            }
+
+
+            Console.WriteLine("Updated list of employees:");
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+        }
+
+    }
+}
